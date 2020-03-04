724f883cb14bfc1e88dfac3142da75dd7b424ab7
meghanasp21 <45370974+meghanasp21@users.noreply.github.com>
2020-03-03 23:44:58 -0800

    added init and len

--- dataloader.py
+++ dataloader.py
@@ -3,17 +3,21 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
+import os
 
 
 class ImageNet():
     
-    def __call__(self, idx):
+    def __init__(self,directory):
+        self.direct=directory
+    
+    def __getitem__(self, idx):
         #get pil image and convert to rgb tensors
         
         pil2tensor = transforms.ToTensor()
         tensor2pil = transforms.ToPILImage()
         
-        pil=Image.open("/datasets/imagenet-ds/train_64x64/"+idx)
+        pil=Image.open(self.direct+idx)
         rgb_image = pil2tensor(pil)
         
         r_image = rgb_image[0]
@@ -25,4 +29,7 @@ class ImageNet():
         grayscale_image = (0.4*r_image + 0.4*g_image + 0.2*b_image).div(3.0)
         
         return(grayscale_image, rgb_image)
+    
+    def __len__(self):
+        return len(os.listdir(self.direct))
         
